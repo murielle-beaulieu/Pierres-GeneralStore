13150e9d8c4724acbe7168eb4f9f3abf16c359ec
murielle-beaulieu <murielle.beaulieu19@gmail.com>
2025-01-28 16:50:43 +1100

    progress on the product page

--- src/components/ItemDetails/ItemDetails.jsx
+++ src/components/ItemDetails/ItemDetails.jsx
@@ -1,16 +1,58 @@
 import { Link } from "react-router";
+import classes from "./ItemDetails.module.scss"
+import Button from "../Button/Button";
+import { useState, useEffect } from "react";
 
 const ItemDetails = ({item}) => {
+  const [qty, setQty] = useState(0)
+  const [cart, setCart] = useState(null);
+  const [itemVariant, setItemVariant] = useState('seeds');
+
+  const decrement = () => {
+    setQty((qty) => qty - 1);
+  }
+
+  const increment = () => {
+    setQty((qty) => qty + 1);
+  }
+
+
+  // const addToCart = () => {
+  //   setCart(item.name);
+  //   console.log(qty,cart);
+  // }
 
   return (
     <section>
-      <img src={item.image} alt={item.name} />
-      <h1>{item.name}</h1>
+      { itemVariant === 'seeds' && <img className={classes.product_img} src={item.image} alt={item.name}/> }
+      { itemVariant === 'seedlings' &&<img className={classes.product_img} src={item.seedling_image} alt={item.name} />}
       <h2>{item.description}</h2>
-      <h3>love ur work</h3>
+      <div>
+        <Button onClick={() => setItemVariant('seeds')} value={'Seed Pack'}/>
+        <Button onClick={() => setItemVariant('seedlings')} value={'Seedling'}/>
+      </div>
+      <div className={classes.quantity}>
+        <Button onClick={decrement} value={'-'}/>
+        <h4>{qty}</h4>
+        <Button onClick={increment} value={'+'}/>
+        <Button onClick={() => window.localStorage.setItem(`${item.id}`,`${qty}`)} value={'Add to Cart'}/>
+      </div>
       <Link to="/">Back</Link>
     </section>
   )
 }
 
 export default ItemDetails;
+
+
+/* Adding an item */
+// window.localStorage.setItem("key", "value")
+
+/* Getting an item */
+// window.localStorage.getItem('Data')
+
+/* Removing an item */
+// window.localStorage.removeItem("Data");
+
+/* Removing all items */
+// window.localStorage.clear();
--- src/services/inventory.js
+++ src/services/inventory.js
@@ -7,12 +7,14 @@ import {
 
 import { db } from '../config/firestore';
 
+// real all
 export const getAllInventory = async () => {
   const collectionRef = collection(db, 'inventory');
   const snapshot = await getDocs(collectionRef);
   return snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
 };
 
+// read one
 export const getInventoryItem = async (id) => {
   const docRef = doc(db, 'inventory', id);
   const snapshot = await getDoc(docRef);
